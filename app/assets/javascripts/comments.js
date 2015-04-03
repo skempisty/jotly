7b49965bd43af599a746914a26c0b785d43d9af5
Stephen Kempisty <stephen.kempisty@gmail.com>
2015-04-03 13:04:49 -0400

    Pre-fixing Javascript loading

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,28 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function(){
+//
+//   // $(".thumbnail").each(function(){
+//   //
+//   //   var $this = $(this);
+//   //   var $img = $this.find('img');
+//   //
+//   //   $img.load(function(){
+//   //
+//   //     var height = $img.height();
+//   //     $this.find('.caption').css("height", 530 - 104 - height);
+//   //   });
+//   //
+//   // });
+//
+  // var capHeight = $("div.caption").height();
+  // var imgHeight = $("div.img").height();
+  //
+  //
+  // $(".thumbnail").height(capHeight + imgHeight);
+  //
+  //
+
+});
