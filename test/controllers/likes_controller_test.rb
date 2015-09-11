eac7295c9fdb9c693b85ebee3fdc29ac89b4943f
Stephen Kempisty <stephen.kempisty@gmail.com>
2015-09-11 15:58:50 -0400

    Added Testing
    added likesController testing

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class LikesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #like
+
+  describe "the like action" do
+    before do
+      session[:current_user] = users(:steve).id
+    end
+    it "should POST a like" do
+      @request.env["HTTP_REFERER"] = '/'
+      post :like, { id: jots(:testJot).id }
+    end
+  end
+
+
+  #unlike
+
+  describe "the unlike action" do
+    before do
+      session[:current_user] = users(:steve).id
+    end
+    it "should POST an unlike" do
+      @request.env["HTTP_REFERER"] = '/'
+      post :unlike, { id: jots(:testJot).id }
+    end
+  end
+
+  #show
+
+  describe "the show action" do
+    before do
+      session[:current_user] = users(:steve).id
+    end
+    it "should GET show likes" do
+      get :show, { id: jots(:testJot).id }
+    end
+  end
 end
