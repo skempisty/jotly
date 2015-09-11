eaf985815fac86c8196586ab29f48fe63cf88123
Stephen Kempisty <stephen.kempisty@gmail.com>
2015-09-11 10:36:53 -0400

    Added admin controller tests
    title

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #edit_users
+
+  describe "the edit_users action" do
+    before do
+      session[:current_user] = users(:steve).id
+    end
+
+    it "should GET the edit_users page" do
+      get :edit_users
+    end
+  end
+
+  #promote_demote
+
+  describe "the promote_demote action" do
+    before do
+      session[:current_user] = users(:steve).id
+    end
+
+    it "should POST change admin status" do
+      @request.env["HTTP_REFERER"] = '/'
+      post :promote_demote, { id: users(:steve).id }
+    end
+  end
+
+  #sticky
+
+  describe "the sticky action" do
+    before do
+      session[:current_user] = users(:steve).id
+    end
+
+    it "should POST change sticky status" do
+      @request.env["HTTP_REFERER"] = '/'
+      post :sticky, { id: jots(:testJot).id }
+    end
+  end
+
 end
