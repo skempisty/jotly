4ad90fd255f6c7f404d58e4c62dcb6e792e0b14a
Stephen Kempisty <stephen.kempisty@gmail.com>
2015-09-17 16:53:48 -0400

    Nothing

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+
+  # #new
+  #
+  # describe "the new action" do
+  #   before do
+  #     session[:current_user] = users(:steve).id
+  #   end
+  #   it "should create" do
+  #     post :create, { jot_id: 1, comment: comments(:testComment).attributes }
+  #   end
   # end
+  #
+  # #create
+  #
+  # describe "the create action" do
+  #   before do
+  #     session[:current_user] = users(:steve).id
+  #   end
+  #   it "should create" do
+  #     post :create, { jot_id: 1, comment: comments(:testComment).attributes }
+  #   end
+  # end
+
 end
