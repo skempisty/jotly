c34e14462e2c0fdbcadc9cb1db7d5a8f0d9f0406
Stephen Kempisty <stephen.kempisty@gmail.com>
2015-10-06 16:55:29 -0400

    Add assertions to sessions_controller_test.rb

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,26 +2,15 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
 
-  # #new
-  #
-  # describe "the new action" do
-  #   before do
-  #     session[:current_user] = users(:steve).id
-  #   end
-  #   it "should create" do
-  #     post :create, { jot_id: 1, comment: comments(:testComment).attributes }
-  #   end
-  # end
-  #
-  # #create
-  #
-  # describe "the create action" do
-  #   before do
-  #     session[:current_user] = users(:steve).id
-  #   end
-  #   it "should create" do
-  #     post :create, { jot_id: 1, comment: comments(:testComment).attributes }
-  #   end
-  # end
+  #new
 
-end
+  describe "the new action" do
+    before do
+      session[:current_user] = users(:steve).id
+    end
+    it "gets new session page" do
+      get :new
+      response.success?.must_equal true
+    end
+  end
+end  
