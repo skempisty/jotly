3cdba06f5dad67c4557c7a7a15dbaaaf48209b17
Stephen Kempisty <stephen.kempisty@gmail.com>
2015-10-06 15:52:45 -0400

    Add assertions to jots_controller_test.rb

--- test/controllers/jots_controller_test.rb
+++ test/controllers/jots_controller_test.rb
@@ -4,95 +4,94 @@ class JotsControllerTest < ActionController::TestCase
 
   #index
 
-  describe "the index action" do
+  describe "index action" do
     before do
       session[:current_user] = users(:steve).id
     end
 
     it "should GET jot index" do
       get :index
+      response.success?.must_equal true
     end
   end
 
   #new
 
-  describe "the new action" do
+  describe "new action" do
     before do
       session[:current_user] = users(:steve).id
     end
 
-    it "should GET new jot" do
+    it "should GET new jot page" do
       get :new
+      response.success?.must_equal true
     end
   end
 
   #create
 
-  describe "the create action" do
+  describe "create action" do
     before do
       session[:current_user] = users(:steve).id
     end
 
-    it "should POST a jot" do
+    it "redirects to jots_path" do
       post :create, { jot: jots(:testJot).attributes }
+      assert_redirected_to (jots_path)
     end
   end
 
   #edit
 
-  describe "the edit action" do
+  describe "edit action" do
     before do
       session[:current_user] = users(:steve).id
     end
 
     it "should GET edit jot" do
-      post :edit, { id: jots(:testJot).id }
+      get :edit, { id: jots(:testJot).id }
+      response.success?.must_equal true
     end
   end
 
   #update
 
-  describe "the update action" do
+  describe "update action" do
     before do
       session[:current_user] = users(:steve).id
     end
 
-    it "should PATCH update a jot" do
-      patch :update, { id: jots(:testJot).id, jot: jots(:testJot).attributes }
-    end
-  end
-
-  #destroy
-
-  describe "the destroy action" do
-    before do
-      session[:current_user] = users(:steve).id
-    end
-    it "should DELETE the jot" do
-      @request.env["HTTP_REFERER"] = '/'
-      delete :destroy, { id: jots(:testJot).id }
+    it "should update the jot" do
+      jot = jots(:testJot)
+      jotInit = jot.content
+      patch :update, { id: jots(:testJot).id,
+        jot: jot.attributes = { content: "This is a test" }}
+      jotFinal = jot.content
+      jotFinal.wont_equal jotInit
     end
   end
 
   #followed
 
-  describe "the followed action" do
+  describe "followed action" do
     before do
       session[:current_user] = users(:steve).id
     end
     it "should GET the followed page" do
       get :followed
+      response.success?.must_equal true
     end
   end
 
   #my_jots
 
-  describe "the my_jots action" do
+  describe "my_jots action" do
     before do
       session[:current_user] = users(:steve).id
     end
     it "should GET the my_jots page" do
       get :my_jots
+      response.success?.must_equal true
     end
   end
 end
